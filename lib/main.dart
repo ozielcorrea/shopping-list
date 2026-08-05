00a57ff2340b67649a36f2666b06778695c79786
Oziel Correa <312325537+ozielcorrea@users.noreply.github.com>
2026-08-05 06:09:37 -0600

    style: format file with LSP

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
